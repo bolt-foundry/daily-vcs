994db1d2ee8c995ddcf17e3ac70d31f1833a6fe0
Randall Bennett <randallb@users.noreply.github.com>
2024-05-11 02:07:44 +0000

    Trigger workers from environment variables

--- packages/packages.ts
+++ packages/packages.ts
@@ -4,4 +4,21 @@ const entrypointPath = Deno.env.get("BF_ENTRYPOINT");
 if (entrypointPath === undefined) {
   throw new Error("Must define BF_ENTRYPOINT environment variable");
 }
-await import(entrypointPath);
+
+const _worker = new Worker(
+  import.meta.resolve(entrypointPath),
+  { type: "module", name: entrypointPath },
+)
+
+// Listen for SIGTERM signal and log a message
+Deno.addSignalListener("SIGTERM", () => {
+  console.log("SIGTERM signal received. Shutting down.");
+});
+
+Deno.addSignalListener("SIGINT", () => {
+  console.log("SIGINT signal received. Shutting down.");
+})
+
+Deno.addSignalListener("SIGHUP", () => {
+  console.log("SIGHUP signal received. Shutting down.");
+})
