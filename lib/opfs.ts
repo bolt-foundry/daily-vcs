43389e03db6250071addb18996a321ec6a3903e6
Justin Carter <github@justicart.com>
2024-05-16 18:20:49 +0000

    rename BfError, rm timeout from toast context

--- lib/opfs.ts
+++ lib/opfs.ts
@@ -1,5 +1,5 @@
 import { getLogger, rxjs } from "deps.ts";
-import { BfError } from "lib/bfError.ts";
+import { BfError } from "lib/BfError.ts";
 const { Subject } = rxjs;
 const logger = getLogger(import.meta);
 
@@ -18,33 +18,40 @@ export class ErrorOpfsNotSupported extends ErrorOpfsGenericError {
 export type FileMetadata = {
   name: string;
   size: number;
-}
+};
 
 export type OpfsProgressEvent = {
   type: "progress";
   file: FileMetadata;
   bytesWritten: number;
   totalBytesWritten: number;
-}
+};
 export type OpfsCompletionEvent = {
   type: "completion";
   file: FileMetadata;
   message: string;
-}
+};
 export type OpfsErrorEvent = {
   type: "error";
   file: FileMetadata;
   error: ErrorOpfsGenericError;
-}
+};
 
-export function streamFileToOpfs(file: File, fileName: string): rxjs.Observable<OpfsProgressEvent | OpfsCompletionEvent | OpfsErrorEvent> {
-  const subject = new Subject<OpfsProgressEvent | OpfsCompletionEvent | OpfsErrorEvent>();
+export function streamFileToOpfs(
+  file: File,
+  fileName: string,
+): rxjs.Observable<OpfsProgressEvent | OpfsCompletionEvent | OpfsErrorEvent> {
+  const subject = new Subject<
+    OpfsProgressEvent | OpfsCompletionEvent | OpfsErrorEvent
+  >();
   const fileMetadata: FileMetadata = { name: fileName, size: file.size };
 
   (async () => {
     try {
       const opfsDirectory = await navigator.storage.getDirectory();
-      const opfsFile = await opfsDirectory.getFileHandle(fileName, { create: true });
+      const opfsFile = await opfsDirectory.getFileHandle(fileName, {
+        create: true,
+      });
 
       const writableStream = await opfsFile.createWritable();
       const reader = file.stream().getReader();
@@ -57,16 +64,29 @@ export function streamFileToOpfs(file: File, fileName: string): rxjs.Observable<
         totalWritten += value.length;
         logger.log(`Written ${totalWritten} bytes`);
 
-        subject.next({ type: "progress", file: fileMetadata, bytesWritten: value.length, totalBytesWritten: totalWritten });
+        subject.next({
+          type: "progress",
+          file: fileMetadata,
+          bytesWritten: value.length,
+          totalBytesWritten: totalWritten,
+        });
       }
 
       await writableStream.close();
       logger.log("File successfully written to Opfs");
 
-      subject.next({ type: "completion", file: fileMetadata, message: "File successfully written to Opfs" });
+      subject.next({
+        type: "completion",
+        file: fileMetadata,
+        message: "File successfully written to Opfs",
+      });
       subject.complete();
     } catch (error) {
-      subject.error({ type: "error", file: fileMetadata, error: new ErrorOpfsGenericError(error.message) });
+      subject.error({
+        type: "error",
+        file: fileMetadata,
+        error: new ErrorOpfsGenericError(error.message),
+      });
     }
   })();
 
@@ -74,13 +94,13 @@ export function streamFileToOpfs(file: File, fileName: string): rxjs.Observable<
 }
 
 export async function clearOpfsStorage(): Promise<void> {
-  if (confirm('Are you sure you want to remove all storage?')) {
+  if (confirm("Are you sure you want to remove all storage?")) {
     try {
       const opfsDirectory = await navigator.storage.getDirectory();
       for await (const entry of opfsDirectory.values()) {
         await opfsDirectory.removeEntry(entry.name, { recursive: true });
       }
-      alert('All storage has been cleared.');
+      alert("All storage has been cleared.");
     } catch (error) {
       alert(`Failed to clear storage: ${error.message}`);
     }
--- packages/bfDs/conexts/BfDsContext.tsx
+++ packages/bfDs/conexts/BfDsContext.tsx
@@ -15,23 +15,21 @@ export type ToastContextType = {
   ToastComponent: ReactNode;
 };
 
-export const ToastContext = createContext<ToastContextType | undefined>(undefined);
+export const ToastContext = createContext<ToastContextType | undefined>(
+  undefined,
+);
 
 export const BfDsProvider = ({ children }: { children: ReactNode }) => {
   const [toastMessage, setToastMessage] = useState<string | ReactNode>("");
-  const [options, setOptions] = useState<UseToastOptions | undefined>(undefined);
+  const [options, setOptions] = useState<UseToastOptions | undefined>(
+    undefined,
+  );
   const [shouldShow, setShouldShow] = useState(false);
 
   function showToast(message: ReactNode, options: UseToastOptions = {}) {
     setToastMessage(message);
     setOptions(options);
     setShouldShow(true);
-
-    if (options.timeout && options.timeout > 0) {
-      setTimeout(() => {
-        setShouldShow(false);
-      }, options.timeout);
-    }
   }
 
   useEffect(() => {
@@ -41,21 +39,30 @@ export const BfDsProvider = ({ children }: { children: ReactNode }) => {
     }
   }, [shouldShow]);
 
-  const value = { showToast, ToastComponent: shouldShow ? (
-    <Toast
-      shouldShow={shouldShow}
-      title={options?.title}
-      timeout={options?.timeout}
-      closeCallback={() => setShouldShow(false)}
-    >
-      {toastMessage}
-    </Toast>
-  ) : null};
+  const value = {
+    showToast,
+    ToastComponent: shouldShow
+      ? (
+        <Toast
+          shouldShow={shouldShow}
+          title={options?.title}
+          timeout={options?.timeout}
+          closeCallback={() => setShouldShow(false)}
+        >
+          {toastMessage}
+        </Toast>
+      )
+      : null,
+  };
 
   return (
     <ToastContext.Provider value={value}>
       {children}
-      {shouldShow && createPortal(value.ToastComponent, document.getElementById("toast-root") as Element)}
+      {shouldShow &&
+        createPortal(
+          value.ToastComponent,
+          document.getElementById("toast-root") as Element,
+        )}
     </ToastContext.Provider>
   );
 };
