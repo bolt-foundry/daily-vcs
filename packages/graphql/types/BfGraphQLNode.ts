d68febbd22d2f21c4cfad22291f5493bbf68d350
Randall Bennett <randallb@users.noreply.github.com>
2024-05-16 17:55:37 +0000

    Fix graphqlnode resolver to get the ID correctly
    tsia

--- packages/graphql/types/BfGraphQLNode.ts
+++ packages/graphql/types/BfGraphQLNode.ts
@@ -3,6 +3,6 @@ import { interfaceType } from "packages/graphql/deps.ts";
 export const BfNodeGraphQLType = interfaceType({
   name: "BfNode",
   definition(t) {
-    t.nonNull.id("id", { description: "Unique identifier for the resource", resolve: (obj) => obj.bfGid });
+    t.nonNull.id("id", { description: "Unique identifier for the resource", resolve: (obj) => obj.id ?? obj.bfGid ?? obj.metadata?.bfGid });
   },
 });
