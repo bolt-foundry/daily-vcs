1e84180b0ab640041923834f4602228735080c4f
Randall Bennett <randall@boltfoundry.com>
2024-06-04 20:25:47 +0000

    Add new bff build-ey command
    Summary:
    
    build!!
    
    Test Plan:

--- infra/bff/friends/build.bff.ts
+++ infra/bff/friends/build.bff.ts
@@ -1,9 +1,16 @@
 import { build } from "infra/build/build.ts";
-// import { buildRelay } from "infra/bff/friends/relay.bff.ts";
 import { register } from "infra/bff/mod.ts";
+import { buildNix } from "infra/bff/friends/nix.bff.ts";
+import { buildRelay } from "infra/bff/friends/relay.bff.ts";
 
 register("build", "Builds the client.", async (_options) => {
-  // await buildRelay();
   await build();
   return 0;
 });
+
+register("build:deploy", "build the client and include building the environment", async (_options) => {
+  await buildNix();
+  await buildRelay();
+  await build();
+  return 0;
+})
--- infra/bff/friends/nix.bff.ts
+++ infra/bff/friends/nix.bff.ts
@@ -1,8 +1,10 @@
-import {
-  runShellCommand,
-} from "infra/bff/shellBase.ts";
+import { runShellCommand } from "infra/bff/shellBase.ts";
 import { register } from "infra/bff/mod.ts";
 
-register("nix", "Builds the current nix system using flake.nix", () => {
+export function buildNix() {
   return runShellCommand(["nix", "build", "--out-link", "build/system"]);
-})
+}
+
+register("nix", "Builds the current nix system using flake.nix", () => {
+  return buildNix();
+});
