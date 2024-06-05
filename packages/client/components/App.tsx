e70f69b293c1ba2d659ba1c0416e2b4ab09476ff
Randall Bennett <randall@boltfoundry.com>
2024-06-05 13:30:15 +0000

    Reorganized app.tsx
    Summary:
    
    In working with bootcampers, I realized app.tsx was more bloated than it needed to be, so i fixed it up to be more readable.
    
    Test Plan:
    👀

--- packages/client/components/App.tsx
+++ packages/client/components/App.tsx
@@ -1,29 +1,17 @@
+import { ErrorBoundary } from "packages/client/components/ErrorBoundary.tsx";
 import { getLogger, React } from "deps.ts";
-
 const logger = getLogger(import.meta);
 
-import { ErrorBoundary } from "packages/client/components/ErrorBoundary.tsx";
-
-// import * as DashboardModule from "packages/client/pages/DashboardPrototype.tsx";
-import { LoginPage } from "packages/client/pages/LoginPage.tsx";
-import { InternalBfPage } from "packages/client/pages/InternalBfPage.tsx";
-// import * as PricingPageModule from "packages/client/pages/PricingPage.tsx";
-import { Marketing } from "packages/client/pages/MarketingPage.tsx";
-import { ProjectPage } from "packages/client/pages/ProjectPage.tsx";
-// import * as ProjectPageOldModule from "packages/client/components/ProjectPageOld.tsx";
-// import * as ReviewPageModule from "packages/client/pages/ReviewPage.tsx";
-// import * as SignupPageModule from "packages/client/pages/SignupPage.tsx";
-import { Demo } from "packages/bfDs/Demo.tsx";
-// import * as ConfirmEmailPageModule from "packages/client/pages/ConfirmEmailPage.tsx";
-// import * as ResetPasswordPageModule from "packages/client/pages/ResetPasswordPage.tsx";
-// import * as LocalVideoPageModule from "packages/client/pages/LocalVideoPage.tsx";
-// import * as FreeTranscribePageModule from "packages/client/pages/FreeTranscribePage.tsx";
-// import * as TermsAndPrivacyPageModule from "packages/client/pages/TermsAndPrivacyPage.tsx";
-// import * as ContentPageModule from "packages/client/pages/ContentPage.tsx";
 import {
   matchRouteWithParams,
   useRouter,
 } from "packages/client/contexts/RouterContext.tsx";
+
+import { LoginPage } from "packages/client/pages/LoginPage.tsx";
+import { InternalBfPage } from "packages/client/pages/InternalBfPage.tsx";
+import { Marketing } from "packages/client/pages/MarketingPage.tsx";
+import { ProjectPage } from "packages/client/pages/ProjectPage.tsx";
+import { Demo } from "packages/bfDs/Demo.tsx";
 import { ProjectNew } from "packages/client/pages/ProjectNew.tsx";
 
 export const routes = new Map([
