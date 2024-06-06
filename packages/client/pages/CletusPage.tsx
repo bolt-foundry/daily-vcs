03ba63466d62b4b383a73ac882e332f631686437
Cletus Igwe <cletus@boltfoundry.com>
2024-06-06 22:44:12 +0000

    add cletus first page
    Summary:
    
    TSIA

---
+++ packages/client/pages/CletusPage.tsx
@@ -0,0 +1,6 @@
+import { React } from "deps.ts";
+import { PageFrame } from "packages/client/components/PageFrame.tsx";
+
+export const CletusPage = () => {
+  return(<PageFrame><h2>Cletus First Page 🥳</h2></PageFrame>)
+};
