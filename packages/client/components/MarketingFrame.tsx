2817eac22617c71a8faca9ec55f634fac6b98a65
Randall Bennett <randall@boltfoundry.com>
2024-06-14 17:22:40 +0000

    Remove envrc and bring back building
    Summary:
    
    Test Plan:

--- packages/client/components/MarketingFrame.tsx
+++ packages/client/components/MarketingFrame.tsx
@@ -98,7 +98,7 @@ export function MarketingFrame(
         </div>
         {showLoginLink && (
           <div className="nav-links mobile-hide" style={styles.rightSide}>
-            <Button href="/login" iconLeft="checkCircle"/>
+            <Button href="/login" iconLeft="clipping"/>
           </div>
         )}
         <div
