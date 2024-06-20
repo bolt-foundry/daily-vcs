89fda3a634d5a604ad454677de90f260a123b910
Randall Bennett <randall@boltfoundry.com>
2024-06-20 16:33:52 +0000

    New watcher that runs on my computer
    Summary:
    
    This automatically ingests files which get put in google, and then creates a project and pings discord.
    
    Test Plan:
    https://github.com/bolt-foundry/bolt-foundry/assets/448694/1a5a6853-8e72-453d-9034-53a5fe93b8d3

---
+++ experimental/randallb/watcher/ingest.ts
@@ -0,0 +1,244 @@
+// https://www.npmjs.com/package/graphql-request
+import { gql, GraphQLClient, rawRequest, request } from "npm:graphql-request";
+import * as cookie from "https://deno.land/std@0.197.0/http/cookie.ts";
+
+const USERNAME = Deno.env.get("BFI_USERNAME") || "";
+const PASSWORD = Deno.env.get("BFI_PASSWORD") || "";
+const GRAPHQL_ENDPOINT = Deno.env.get("BFI_GRAPHQL_ENDPOINT") || "";
+const NOTION_ENDPOINT = Deno.env.get("BFI_NOTION_ENDPOINT") || "";
+const NOTION_PARENT = Deno.env.get("BFI_NOTION_PARENT") || "";
+const FFMPEG_ARGS_EXCEPT_INPUT_AND_OUTPUT =
+  Deno.env.get("BFI_FFMPEG_ARGS_EXCEPT_INPUT_AND_OUTPUT") ||
+  "-vf crop=ih*(16/9):ih,scale=1920:1080 -c:v h264_videotoolbox -b:v 7M -c:a aac -b:a 128k -f segment -segment_time 1800 -reset_timestamps 1";
+const client = new GraphQLClient(GRAPHQL_ENDPOINT);
+
+export async function processFile(filePath: string) {
+  console.log("processing file", filePath);
+  const videoChunks = createVideoChunksForFilePath(filePath);
+  const files = [];
+  for await (const chunk of videoChunks) {
+    const project = processVideoChunk(chunk);
+    files.push(project);
+  }
+  console.log(files);
+}
+
+async function* createVideoChunksForFilePath(filePath: string) {
+  console.log(`creating video chunks for file ${filePath}`);
+  const tmpDir = await Deno.makeTempDir();
+  const outputFilePath = `${tmpDir}/output_%03d.mp4`;
+  const ffmpegArgs = [
+    "-i",
+    filePath,
+    ...FFMPEG_ARGS_EXCEPT_INPUT_AND_OUTPUT.split(" "),
+    outputFilePath,
+  ];
+
+  const cmd = new Deno.Command("ffmpeg", {
+    args: ffmpegArgs,
+    env: Deno.env.toObject(),
+    stdout: "piped",
+    stderr: "piped", // Also capture stderr for more detailed logs
+  });
+
+  const ffmpegProcess = cmd.spawn();
+  let previousSegmentPath = null;
+
+  for await (const chunk of ffmpegProcess.stderr) {
+    const outputLine = new TextDecoder().decode(chunk);
+    if (outputLine.includes("time=")) {
+      console.log(outputLine);
+    }
+    const newFileMatch = outputLine.match(
+      /Opening '(.+?\/output_\d+\.mp4)' for writing/,
+    );
+
+    if (newFileMatch) {
+      const segmentPath = newFileMatch[1];
+
+      if (previousSegmentPath) {
+        console.log(`finished writing ${previousSegmentPath}`);
+        yield previousSegmentPath; // Yield when the previous segment is complete
+      }
+
+      console.log(`new file ${segmentPath}`);
+      previousSegmentPath = segmentPath;
+    }
+  }
+
+  // Yield the final segment after the process is complete
+  if (previousSegmentPath) {
+    console.log(`finished writing ${previousSegmentPath}`);
+    yield previousSegmentPath;
+  }
+
+  const output = await ffmpegProcess.status;
+  if (output.success) {
+    console.log("Finished processing", filePath);
+  } else {
+    console.error("Failed to process", filePath);
+  }
+}
+
+async function uploadToS3(filePath: string, s3Url: string) {
+  console.log(`uploading ${filePath} to ${s3Url}`);
+
+  // Read the file into a buffer
+  const fileBuffer = await Deno.readFile(filePath);
+
+  const response = await fetch(s3Url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Content-Length": fileBuffer.length.toString(), // Adding Content-Length header
+    },
+    body: fileBuffer,
+  });
+
+  if (response.ok) {
+    console.log("File uploaded successfully:", filePath);
+  } else {
+    console.error("Error uploading file:", response.statusText);
+    console.error("HTTP status code:", response.status);
+    const errorBody = await response.text();
+    console.error("Error details:", errorBody);
+  }
+}
+
+async function processVideoChunk(chunkPath: string) {
+  console.log("processing video chunk", chunkPath);
+  const project = await createProjectInBf(chunkPath);
+  await uploadToS3(chunkPath, project.trackUploadUrl);
+  console.log(
+    "uploaded to s3",
+    chunkPath,
+    `https://justin.boltfoundry.wtf${project.url}`,
+  );
+
+  // append the link to a file in /tmp/files_uploaded.txt
+  const uploadedLink = `https://justin.boltfoundry.wtf${project.url}`;
+  try {
+    await Deno.writeTextFile("/tmp/files_uploaded.txt", `${uploadedLink}\n`, {
+      append: true,
+    });
+  } catch (err) {
+    console.error("Error appending link to /tmp/files_uploaded.txt", err);
+  }
+  await notifyDiscord(`Uploaded video to ${uploadedLink}`);
+  // const notionLink = await createTaskInNotion(project);
+  // console.log(`successfully created ${notionLink}`);
+}
+
+export function notifyDiscord(content: string) {
+  console.log("notifying discord");
+  const discordWebhookUrl = Deno.env.get("BFI_DISCORD_WEBHOOK_URL");
+  if (!discordWebhookUrl) {
+    console.error("Discord webhook URL not found");
+    return;
+  }
+
+  const payload = {
+    content,
+  };
+
+  return fetch(discordWebhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Error sending Discord webhook:", response.statusText);
+      } else {
+        console.log("Discord webhook sent successfully");
+      }
+    })
+    .catch((error) => {
+      console.error("Error sending Discord webhook:", error);
+    });
+}
+
+export async function createProjectInBf(chunkPath) {
+  // get current access token
+  const headers = await getHeaders();
+  // create project on bf
+  // get file from hard drive
+  // stream file to s3
+  // when stream is done return project
+  const mutation = gql`
+    mutation FileUploadCreateNewProjectMutation(
+      $name: String!
+      $height: Int!
+      $width: Int!
+      $language: String!
+      $model: String!
+      $usesOriginPrivateFileSystem: Boolean!) 
+
+      {createProject
+      (name: $name, 
+      height: $height, 
+      width: $width, 
+      language: $language, 
+      model: $model, 
+      usesOriginPrivateFileSystem: $usesOriginPrivateFileSystem) {
+        node {
+          trackUploadUrl
+          url
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    name: "test",
+    height: 1080,
+    width: 1920,
+    language: "en",
+    model: "assemblyai",
+    usesOriginPrivateFileSystem: false,
+  };
+
+  const returned = await client.request(mutation, variables, headers);
+  return returned.createProject.node;
+}
+
+function createTaskInNotion(project) {
+  const path = project.url; // or something
+  // magically call notion's api.
+}
+
+async function getHeaders() {
+  // login mutation for graphql
+  const loginMutation = gql`
+  mutation LoginFormLoginMutation(
+  $email: String!
+  $password: String!
+) {
+  login(email: $email, password: $password) {
+    name
+    id
+  }
+}
+`;
+  const { headers } = await rawRequest(GRAPHQL_ENDPOINT, loginMutation, {
+    email: USERNAME,
+    password: PASSWORD,
+  });
+
+  const setCookieHeader = headers.get("set-cookie");
+  const returnableHeaders = new Headers();
+  if (setCookieHeader) {
+    const cookies = setCookieHeader.split(",");
+    const cookieEntry = [];
+    cookies.forEach((cookie) => {
+      const [nameValue, ...attributes] = cookie.split(";");
+      const [name, value] = nameValue.split("=");
+      cookieEntry.push(`${name}=${value}`);
+    });
+    returnableHeaders.append("cookie", cookieEntry.join(";"));
+  }
+
+  return returnableHeaders;
+}
---
+++ experimental/randallb/watcher/watcher.ts
@@ -0,0 +1,97 @@
+#! /usr/bin/env -S deno run -A
+
+import { dirname, join } from "https://deno.land/std@0.212.0/path/mod.ts";
+import { notifyDiscord, processFile } from "./ingest.ts";
+
+const FOLDER_PATH =
+  "/Users/randallb/Library/CloudStorage/GoogleDrive-randall@boltfoundry.com/Shared drives/Customers";
+const PATH_FOR_INGEST = "01 New Videos";
+const PATH_FOR_ARCHIVE = "Z Processed Videos";
+const MAX_RETRIES = 15;
+const events = Deno.watchFs(FOLDER_PATH, { recursive: true });
+console.log(`watching ${FOLDER_PATH}`);
+const seenFiles = new Set();
+notifyDiscord(`BFI watcher running on ${Deno.env.get("HOSTNAME")}`)
+Deno.addSignalListener("SIGTERM", async () => {
+  await notifyDiscord(`BFI watcher ${Deno.env.get("HOSTNAME")} shutting down`);
+});
+Deno.addSignalListener("SIGINT", async () => {
+  await notifyDiscord(`BFI watcher ${Deno.env.get("HOSTNAME")} shutting down`);
+  Deno.exit();
+});
+globalThis.addEventListener("unload", async () => {
+  await notifyDiscord(`BFI watcher ${Deno.env.get("HOSTNAME")} shutting down`);
+});
+for await (const event of events) {
+  if (event.paths[0].includes(".DS_Store")) {
+    continue;
+  }
+
+  if (event.kind === "modify") {
+    copyAndMove(event.paths[0]);
+  }
+}
+
+
+
+async function copyAndMove(path) {
+    let tmpFile;
+  try {
+    const stat = await Deno.stat(path);
+
+    if (stat.isFile && !seenFiles.has(path) && path.includes(PATH_FOR_INGEST)) {
+      seenFiles.add(path);
+      tmpFile = await Deno.makeTempFile({ prefix: "copy_" });
+      console.log("copying", path, tmpFile);
+      const success = await copyFileWithRetry(path, tmpFile);
+
+      if (success) {
+        const fileDirectory = dirname(path);
+        const filename = path.split("/").pop();
+        const archiveDirectory = `${dirname(fileDirectory)}/${PATH_FOR_ARCHIVE}`;
+        const archivePath = `${archiveDirectory}/${filename}`;
+        await Deno.rename(path, archivePath);
+        console.log("processing");
+        notifyDiscord(`Starting to process ${path.split(FOLDER_PATH)[1]}`)
+        await processFile(tmpFile);
+        console.log("done");
+      } else {
+        console.log("failed to copy file after retries", path);
+      }
+    }
+  } catch (e) {
+    console.log("lol", e);
+  } finally {
+    if (tmpFile) {
+      await Deno.remove(tmpFile);
+    }
+  }
+}
+
+async function copyFileWithRetry(
+  src: string,
+  dest: string,
+  maxRetries: number = MAX_RETRIES,
+): Promise<boolean> {
+  let attempt = 0;
+  let delay = 1000; // initial delay in milliseconds
+
+  while (attempt < maxRetries) {
+    try {
+      await Deno.copyFile(src, dest);
+      return true; // success
+    } catch (e) {
+      if (e.name === "TimedOut") {
+        console.log(`Attempt ${attempt + 1} failed: ${e.message}`);
+        attempt++;
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        delay *= 2; // exponential backoff
+      } else {
+        console.error(`Unexpected error encountered: ${e.message}`);
+        return false;
+      }
+    }
+  }
+
+  return false; // failed after max retries
+}
