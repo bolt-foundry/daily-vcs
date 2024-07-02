ca4c7f3e55d52c0adc41a1c0f62e66166e5bc249
Justin Carter <justin@boltfoundry.com>
2024-07-02 22:19:12 +0000

    fix censoring for real
    Summary:
    
    Test Plan:

--- aws/client/hooks/featureFlagHooks.tsx
+++ aws/client/hooks/featureFlagHooks.tsx
@@ -9,6 +9,9 @@ import { useAppEnvironment } from "aws/client/contexts/AppEnvironmentContext.tsx
 export function useFeatureVariant(
   variant: keyof FeatureVariants,
 ): FeatureVariant<typeof variant> {
+  if (variant === "sv_swear_words") {
+    return tempSwears;
+  }
   const { featureVariants } = useAppEnvironment();
 
   return featureVariants[variant];
@@ -27,3 +30,31 @@ export function useFeatureTier(
   const flag = `gating_${tier}` as keyof FeatureFlags;
   return useFeatureFlag(flag);
 }
+
+const tempSwears = [
+  "ass",
+  "bitch",
+  "cock",
+  "cunt",
+  "dick",
+  "dickhead",
+  "douche",
+  "fuck",
+  "fucked",
+  "fucker",
+  "fucking",
+  "fucks",
+  "pussy",
+  "shit",
+  "bitching",
+  "motherfucker",
+  "motherfuckers",
+  "goddammit",
+  "fucker",
+  "retarded",
+  "asshole",
+  "shitty",
+  "nigga",
+  "nigger",
+  "shittier",
+];
