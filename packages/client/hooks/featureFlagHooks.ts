ceea6a72da692472de7be984a2b1a597c509d315
Justin Carter <justin@boltfoundry.com>
2024-07-02 14:41:53 +0000

    fix censoring
    Summary:
    
    Test Plan:

--- packages/client/hooks/featureFlagHooks.ts
+++ packages/client/hooks/featureFlagHooks.ts
@@ -5,5 +5,10 @@ export function useFeatureTier(str: string) {
   return true;
 }
 export function useFeatureVariant(str: string) {
+  if (str === "sv_swear_words") {
+    return tempSwears;
+  }
   return true;
 }
+
+const tempSwears = ["ass", "bitch", "cock", "cunt", "dick", "dickhead", "douche", "fuck", "fucked", "fucker", "fucking", "fucks", "pussy", "shit", "bitching", "motherfucker", "motherfuckers", "goddammit", "fucker", "retarded", "asshole", "shitty", "nigga", "nigger", "shittier"];
