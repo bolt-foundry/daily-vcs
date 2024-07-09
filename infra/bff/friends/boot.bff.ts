9e78fc71f1166df3eff576d10568bdebd86b12d4
Randall Bennett <randall@boltfoundry.com>
2024-07-09 15:52:08 +0000

    Warm bff cache on boot so things run faster probably™
    Summary:
    
    Test Plan:

--- infra/bff/friends/boot.bff.ts
+++ infra/bff/friends/boot.bff.ts
@@ -1,9 +1,11 @@
 import { register } from "infra/bff/mod.ts";
+import { runShellCommand } from "infra/bff/shellBase.ts";
 
 register(
   "boot",
   "initializes the repl with applicable options when the repl boots up",
   async () => {
+    await runShellCommand(['bff']);
     return await 0;
   },
 );
