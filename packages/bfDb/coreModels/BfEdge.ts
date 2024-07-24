5b5da72991f1b2ed314ae08fa788d37d6222aff2
Randall Bennett <randall@boltfoundry.com>
2024-07-24 18:24:18 +0000

    Make BF Edge stupid
    Summary:
    
    Test Plan:

--- packages/bfDb/coreModels/BfEdge.ts
+++ packages/bfDb/coreModels/BfEdge.ts
@@ -1,8 +1,6 @@
 import { BfNode } from "packages/bfDb/coreModels/BfNode.ts";
 import { CreationMetadata } from "packages/bfDb/classes/BfBaseModelMetadata.ts";
 import { BfGid, BfSid, BfTid } from "packages/bfDb/classes/BfBaseModelIdTypes.ts";
-import { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
-import { BfModelErrorNotImplemented } from "packages/bfDb/classes/BfModelError.ts";
 
 export type BfEdgeRequiredProps = Record<string, never>;
 
@@ -10,10 +8,11 @@ export type BfEdgeOptionalProps = {
   action?: string;
 };
 
-type AssocCreationMetadata = CreationMetadata & {
+type EdgeCreationMetadata = CreationMetadata & {
   bfTid: BfTid | BfGid;
   bfSid: BfSid | BfGid;
 };
+
 
 export class BfEdge<
   ChildRequiredProps extends BfEdgeRequiredProps = BfEdgeRequiredProps,
@@ -21,16 +20,8 @@ export class BfEdge<
 > extends BfNode<
   ChildRequiredProps,
   ChildOptionalProps,
-  AssocCreationMetadata
+  EdgeCreationMetadata
 > {
-  protected static isSorted = true;
   __typename = "BfEdge";
 
-  static findAllForParent(_currentViewer: BfCurrentViewer, _bfSid: BfSid) {
-    throw new BfModelErrorNotImplemented("findAllForParent");
-  }
-
-  static findAllForTarget(_currentViewer: BfCurrentViewer, _bfTid: BfTid) {
-    throw new BfModelErrorNotImplemented("findAllForTarget");
-  }
 }
