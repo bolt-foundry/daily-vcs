2d2e86fb41c099e0fdbfd3ff41a5edbb0f1e04ce
Randall Bennett <randall@boltfoundry.com>
2024-07-30 17:42:20 +0000

    Rename bff deploy to bff land
    Summary:
    
    tsia
    
    Test Plan:

--- infra/bff/friends/deploy.bff.ts
+++ infra/bff/friends/deploy.bff.ts
@@ -13,49 +13,55 @@ function parseLogInfo(loginfo: string) {
   return { commitHash, author, date, summary };
 }
 
+async function land() {
+  const ghToken = await runShellCommandWithOutput([
+    "replit-git-askpass",
+    "Password for 'https://token@github.com': ",
+  ]);
+  if (ghToken.trim() === "no GitHub token available") {
+    console.error("No token, proceeding anyway.");
+  }
+  console.log(
+    "This command will overwrite any changes you have made to the current branch. ctrl + c now. You have 5 seconds",
+  );
+  await runShellCommand(["sleep", "5"]);
+  await runShellCommand(["sl", "pull"], { GH_TOKEN: ghToken });
+  await runShellCommand(["sl", "goto", "main", "--clean"], {
+    GH_TOKEN: ghToken,
+  });
+  console.log(
+    "We will automatically make a git commit in 5 seconds. Please cancel if you don't want this.",
+  );
+  await runShellCommand(["sleep", "5"]);
+  const logInfoString = await runShellCommandWithOutput([
+    "sl",
+    "log",
+    "-l",
+    "1",
+  ]);
+  await runShellCommand(["git", "add", "."]);
+  const logInfo = parseLogInfo(logInfoString);
+  await runShellCommand([
+    "git",
+    "commit",
+    "-a",
+    "--author",
+    logInfo.author,
+    "--date",
+    logInfo.date,
+    "-m",
+    logInfo.summary,
+  ]);
+  await Deno.remove(Deno.env.get("HISTFILE")!);
+  return 0;
+};
 register(
   "deploy",
+  "Deprecated, use land instead",
+  land
+);
+register(
+  "land",
   "Pulls changes down. WILL OVERWRITE ANY CURRENT CHANGES. WILL ALSO MAKE A GIT COMMIT.",
-  async () => {
-    const ghToken = await runShellCommandWithOutput([
-      "replit-git-askpass",
-      "Password for 'https://token@github.com': ",
-    ]);
-    if (ghToken.trim() === "no GitHub token available") {
-      console.error("No token, proceeding anyway.");
-    }
-    console.log(
-      "This command will overwrite any changes you have made to the current branch. ctrl + c now. You have 5 seconds",
-    );
-    await runShellCommand(["sleep", "5"]);
-    await runShellCommand(["sl", "pull"], { GH_TOKEN: ghToken });
-    await runShellCommand(["sl", "goto", "main", "--clean"], {
-      GH_TOKEN: ghToken,
-    });
-    console.log(
-      "We will automatically make a git commit in 5 seconds. Please cancel if you don't want this.",
-    );
-    await runShellCommand(["sleep", "5"]);
-    const logInfoString = await runShellCommandWithOutput([
-      "sl",
-      "log",
-      "-l",
-      "1",
-    ]);
-    await runShellCommand(["git", "add", "."]);
-    const logInfo = parseLogInfo(logInfoString);
-    await runShellCommand([
-      "git",
-      "commit",
-      "-a",
-      "--author",
-      logInfo.author,
-      "--date",
-      logInfo.date,
-      "-m",
-      logInfo.summary,
-    ]);
-    await Deno.remove(Deno.env.get("HISTFILE")!);
-    return 0;
-  },
+  land
 );
