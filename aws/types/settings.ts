3bcc5111a9f9ff4df5f640d015900e344bcdb746
Randall Bennett <randall@boltfoundry.com>
2024-08-07 18:36:03 +0000

    Comment out image preloader
    Summary:
    
    Test Plan:

--- aws/types/settings.ts
+++ aws/types/settings.ts
@@ -2,7 +2,7 @@
  * This is used on multiple models to store project settings
  */
 
-import { imagePreloads } from "packages/vcs/preloads.js";
+const imagePreloads = {};
 
 export type WatermarkLogoType = keyof typeof imagePreloads;
 export type WatermarkPositionType =
