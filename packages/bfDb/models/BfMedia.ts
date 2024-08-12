6950ca1937f06f174191e1a801bfb6d0d3c043ab
Randall Bennett <randall@boltfoundry.com>
2024-08-12 21:49:30 +0000

    Add example method to BfMedia so we can test bf jobs
    Summary:
    
    Test Plan:

--- packages/bfDb/models/BfMedia.ts
+++ packages/bfDb/models/BfMedia.ts
@@ -1,5 +1,8 @@
 import { BfNode } from "packages/bfDb/coreModels/BfNode.ts";
 import { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
+import { getLogger } from "deps.ts";
+
+const logger = getLogger(import.meta);
 
 export type BfMediaProps = {
   filename: string;
@@ -11,5 +14,9 @@ export class BfMedia extends BfNode<BfMediaProps> {
       bfOid: bfCurrentViewer.organizationBfGid,
     });
     return media;
+  }
+
+  example() {
+    logger.info("this is an example job", this.currentViewer);
   }
 }
